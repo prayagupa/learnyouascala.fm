baf7adcffd9c70c8249afd480904112ce28d15d1
prayagupd <prayag.upd@gmail.com>
2016-12-06 22:51:23 -0800

    add data & abstraction readme

--- Lesson4_Data_Abstraction/src/test/scala/objsets/TweetSetSuite.scala
+++ Lesson4_Data_Abstraction/src/test/scala/objsets/TweetSetSuite.scala
@@ -9,16 +9,16 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class TweetSetSuite extends FunSuite {
   trait TestSets {
-    val set1 = new Empty
-    val set2 = set1.incl(new Tweet("a", "a body", 20))
-    val set3 = set2.incl(new Tweet("b", "b body", 20))
+    val set1 = new EmptySet
+    val set2 = set1.include(new Tweet("a", "a body", 20))
+    val set3 = set2.include(new Tweet("b", "b body", 20))
 
     val c = new Tweet("c", "c body", 7)
     val d = new Tweet("d", "d body", 9)
 
-    val set4c = set3.incl(c)
-    val set4d = set3.incl(d)
-    val set5 = set4c.incl(d)
+    val set4c = set3.include(c)
+    val set4d = set3.include(d)
+    val set5 = set4c.include(d)
   }
 
   def asSet(tweets: TweetSet): Set[Tweet] = {
